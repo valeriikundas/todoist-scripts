f525e5ee6d3fd8bbfcb74f0f0ffd56cbaad1e0c2
Valerii <valeriykundas@gmail.com>
2023-11-25 22:32:52 +0200

    ops: cron jobs

--- api.go
+++ api.go
@@ -2,12 +2,47 @@ package api
 
 import (
 	"context"
-	"log"
 	"time"
 
+	"encore.dev/cron"
 	todoist "github.com/valeriikundas/todoist-scripts/todoist_utils"
 )
 
+var secrets struct {
+	// FIXME: move from .env here
+	TodoistApiToken string
+}
+
+// Send Telegram message with projects that has too many and zero active tasks.
+var _ = cron.NewJob("incorrect-projects-notifier", cron.JobConfig{
+	Title:    "Send Telegram message with projects that has too many and zero active tasks",
+	Schedule: "0 8 * * *",
+	Endpoint: GetIncorrectProjectsEndpoint,
+})
+
+// Move tasks from `Inbox` project to `inbox_archive` if they are older than 3 days.
+var _ = cron.NewJob("older-tasks-archivator", cron.JobConfig{
+	Title:    "Move tasks from `Inbox` project to `inbox_archive` if they are older than 3 days",
+	Schedule: "0 8 * * *",
+	Endpoint: ArchiveOlderTasksEndpoint,
+})
+
+// ==================================================================
+
+// Next steps
+//
+// 1. Deploy your application to the cloud
+//
+//     git add -A .
+//     git commit -m 'Commit message'
+//     git push encore
+//
+// 2. To continue exploring Encore, check out one of these topics:
+//
+//    Building a Slack bot:  https://encore.dev/docs/tutorials/slack-bot
+//    Building a REST API:   https://encore.dev/docs/tutorials/rest-api
+//    Using SQL databases:   https://encore.dev/docs/develop/databases
+//    Authenticating users:  https://encore.dev/docs/develop/auth
 
 //encore:api public path=/projects/incorrect
 func GetIncorrectProjectsEndpoint(ctx context.Context) (*IncorrectResponse, error) {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/valeriikundas/todoist-scripts
 
 go 1.21.4
 
-require github.com/google/uuid v1.4.0
+require (
+	encore.dev v1.27.0
+	github.com/google/uuid v1.4.0
+)
