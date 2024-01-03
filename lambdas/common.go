e34acfb933a51ac8e5d7997ef42eb55a78674e69
Valerii <valeriykundas@gmail.com>
2024-01-04 00:16:16 +0200

    feat: extract common lambda functions code

---
+++ lambdas/common.go
@@ -0,0 +1,114 @@
+package lambda_common
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+	"github.com/aws/jsii-runtime-go"
+	"github.com/pkg/errors"
+	"log"
+	"os"
+)
+
+func Run[R any](f func(secrets *Secrets) (*R, error)) {
+	runWithEnv(withHandler(f))
+}
+
+func withHandler[R any](f func(secrets *Secrets) (*R, error)) func(
+	ctx context.Context,
+	request events.APIGatewayProxyRequest,
+) (events.APIGatewayProxyResponse, error) {
+	return func(
+		ctx context.Context,
+		request events.APIGatewayProxyRequest,
+	) (events.APIGatewayProxyResponse, error) {
+		return withSetup(f)
+	}
+}
+
+func runWithEnv(f func(ctx context.Context, request events.APIGatewayProxyRequest) (
+	events.APIGatewayProxyResponse,
+	error,
+)) {
+	runtimeApi := os.Getenv("AWS_LAMBDA_RUNTIME_API")
+	if runtimeApi == "" {
+		resp, err := f(context.TODO(), events.APIGatewayProxyRequest{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Print(resp)
+		return
+	}
+
+	lambda.Start(f)
+}
+
+func withSetup[R any](f func(secrets *Secrets) (R, error)) (
+	events.APIGatewayProxyResponse,
+	error,
+) {
+	log.SetFlags(log.LstdFlags | log.Llongfile)
+
+	secrets, err := readSecrets()
+	if err != nil {
+		return empty500Response(), errors.Wrap(err, "failed to read Secrets")
+	}
+
+	resp, err := f(secrets)
+	if err != nil {
+		return empty500Response(), err
+	}
+
+	return marshall[R](resp)
+}
+
+func empty500Response() events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{StatusCode: 500,
+		Body: "",
+	}
+}
+
+const region = "eu-central-1"
+
+type Secrets struct {
+	TodoistApiToken  string
+	TelegramApiToken string
+	TelegramUserID   string
+}
+
+func readSecrets() (*Secrets, error) {
+	config := aws.NewConfig().WithRegion(region).WithCredentialsChainVerboseErrors(true)
+	sess := session.Must(session.NewSession(config))
+	secretsManager := secretsmanager.New(sess, &aws.Config{})
+
+	secretsOutput, err := secretsManager.GetSecretValue(&secretsmanager.GetSecretValueInput{
+		SecretId: jsii.String("gtd-Secrets"),
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get secret value")
+	}
+
+	var secrets *Secrets
+	err = json.Unmarshal([]byte(*secretsOutput.SecretString), secrets)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal secret")
+	}
+
+	return secrets, nil
+}
+
+func marshall[R any](resp R) (events.APIGatewayProxyResponse, error) {
+	b, err := json.Marshal(resp)
+	if err != nil {
+		return empty500Response(), errors.Wrap(err, "failed to marshal response")
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(b),
+	}, nil
+}
--- lambdas/limit-do-now-tasks/main.go
+++ lambdas/limit-do-now-tasks/main.go
@@ -1,57 +1,17 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/secretsmanager"
-	"github.com/aws/jsii-runtime-go"
 	"github.com/pkg/errors"
 	"github.com/valeriikundas/todoist-scripts/api"
-	"log"
+	"github.com/valeriikundas/todoist-scripts/lambdas"
 	"os"
 	"strings"
 )
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.SetFlags(log.LstdFlags | log.Llongfile)
-
-	region := "eu-central-1"
-	config := aws.NewConfig().WithRegion(region).WithCredentialsChainVerboseErrors(true)
-	session := session.Must(session.NewSession(config))
-	secretsManager := secretsmanager.New(session, &aws.Config{})
-
-	secretsOutput, err := secretsManager.GetSecretValue(&secretsmanager.GetSecretValueInput{
-		SecretId: jsii.String("gtd-secrets"),
-	})
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       "",
-		}, errors.Wrap(err, "failed to get secret value")
-	}
-
-	var secrets struct {
-		TodoistApiToken  string
-		TelegramApiToken string
-		TelegramUserID   string
-	}
-	err = json.Unmarshal([]byte(*secretsOutput.SecretString), &secrets)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       "",
-		}, errors.Wrap(err, "failed to unmarshal secret")
-	}
-
+func f(secrets *lambda_common.Secrets) (*api.IncorrectResponse, error) {
 	excludeFromZeroProjectsString, ok := os.LookupEnv("ExcludeFromZeroProjectsList")
 	if !ok {
-		return events.APIGatewayProxyResponse{StatusCode: 500,
-			Body: "",
-		}, errors.New("ExcludeFromZeroProjectsList environment variable is not set")
+		return nil, errors.New("ExcludeFromZeroProjectsList environment variable is not set")
 	}
 	excludeFromZeroProjectsList := strings.Split(excludeFromZeroProjectsString, ";")
 
@@ -62,36 +22,12 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 		excludeFromZeroProjectsList,
 	)
 	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       "",
-		}, errors.Wrap(err, "failed to get incorrect projects")
+		return nil, errors.Wrap(err, "failed to get incorrect projects")
 	}
 
-	b, err := json.Marshal(resp)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       "",
-		}, errors.Wrap(err, "failed to marshal response")
-	}
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       string(b),
-	}, nil
+	return resp, nil
 }
 
 func main() {
-	runtimeApi := os.Getenv("AWS_LAMBDA_RUNTIME_API")
-	if runtimeApi == "" {
-		resp, err := handler(context.TODO(), events.APIGatewayProxyRequest{})
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Print(resp)
-		return
-	}
-
-	lambda.Start(handler)
+	lambda_common.Run(f)
 }
